b4e3d9788d5e91645bbd9fcd76e509cc35fbfa77
bryn <alan.burt@mackillop.act.edu.au>
2019-10-25 12:45:22 +1100

    Start Code
    Preload Stuff And Scoretext

--- game.js
+++ game.js
@@ -1,10 +1,23 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'theGame', { preload: preload, create: create, update: update });
 
+var score, scoreText;
+
 function preload() {
+  /* game.load.image('alien', 'assets/alien.png');
+  game.load.image('spaceship', 'assets/spaceship.png');
+  game.load.image('background', 'assets/background.png');
+  game.load.image('ground', 'assets/ground.png');
+  game.load.image('heart', 'assets/heart.png'); */
 }
 
 function create() {
+  score = 0;
+  scoreText = game.add.text(game.world.width / 2, 16, "Score: 0", {fontSize: '32px', fill: '#ffffff'});
+  scoreText.anchor.setTo(0.5, 0);
+
+
 }
 
 function update() {
+  scoreText.text = "Score: " + score;
 }
