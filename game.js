bc139911490982385d1daf1aca9b9446f4143941
bryn <alan.burt@mackillop.act.edu.au>
2019-10-30 11:49:31 +1100

    Added Player and Background
    Added player and background code
    added a body to player and added keyboard inputs

--- game.js
+++ game.js
@@ -2,15 +2,31 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'theGame', { preload: preloa
 
 var score, scoreText;
 
+var player;
+
 function preload() {
-  /* game.load.image('alien', 'assets/alien.png');
-  game.load.image('spaceship', 'assets/spaceship.png');
-  game.load.image('background', 'assets/background.png');
-  game.load.image('ground', 'assets/ground.png');
-  game.load.image('heart', 'assets/heart.png'); */
+  game.load.image('alien', 'assets/Alien.png');
+  game.load.image('spaceship', 'assets/Spaceship.png');
+  game.load.image('background', 'assets/Background.png');
+  //game.load.image('ground', 'assets/ground.png');
+  //game.load.image('heart', 'assets/heart.png');
 }
 
 function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  game.add.sprite(0, 0, "background");
+
+  player = game.add.sprite(50, (game.world.height / 2) - 16, "alien");
+  player.scale.setTo(2,2);
+  game.physics.arcade.enable(player);
+  player.body.collideWorldBounds = true;
+  player.body.immovable = true;
+
+  this.cursors = this.input.kerboard.createCursorKeys();
+  this.KeyW = this.input.keyboard.addKey(Phaser.KeyCode.W);
+  this.KeyS = this.input.keyboard.addKey(Phaser.KeyCode.S);
+
   score = 0;
   scoreText = game.add.text(game.world.width / 2, 16, "Score: 0", {fontSize: '32px', fill: '#ffffff'});
   scoreText.anchor.setTo(0.5, 0);
