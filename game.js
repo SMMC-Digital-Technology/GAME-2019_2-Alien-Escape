b990c103db0e6cd102e1c89681121fee79f7b823
bryn <alan.burt@mackillop.act.edu.au>
2019-11-18 14:21:26 +1100

    Created red alien sprite and code
    Made a red alien sprite and code it in for the player to flash between the two different sprites when the spaceship hits it. Bryn

--- game.js
+++ game.js
@@ -16,27 +16,35 @@ var MAX_HEALTH = 3;
 var healthIcons = [];
 
 function preload() {
-  game.load.image('alien', 'assets/Alien.png');
+  //load assets
   game.load.image('spaceship', 'assets/Spaceship.png');
   game.load.image('background', 'assets/Background.png');
   game.load.image('heart', 'assets/heart.png');
+  game.load.image('alienRed', 'assets/AlienRed.png');
+  game.load.spritesheet('alien', 'assets/AlienSheet.png', 21, 38);
 }
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
+  //add background into the game
   game.add.image(0, 0, "background");
 
+  //add player into the game and give it features
   player = game.add.sprite(50, (game.world.height / 2) - 16, "alien");
   player.scale.setTo(2,2);
   game.physics.arcade.enable(player);
   player.body.collideWorldBounds = true;
   player.body.immovable = true;
+  player.animations.add('jetpack');
+  player.animations.play('jetpack', 10, true);
 
+  //add keyboard functionality
   this.cursors = this.input.keyboard.createCursorKeys();
   this.keyW = this.input.keyboard.addKey(Phaser.KeyCode.W);
   this.keyS = this.input.keyboard.addKey(Phaser.KeyCode.S);
 
+  //make spaceship group and add each spaceship into it
   spaceships = game.add.group();
   spaceships.enableBody = true;
   speedMultiplier = 1;
@@ -47,7 +55,7 @@ function create() {
     spaceship.body.velocity.x = -spaceshipSpeed * spacetime * speedMultiplier;
   }
 
-
+  //add health icons
   health = MAX_HEALTH;
   for (var i=0; i < MAX_HEALTH; i++){
     healthIcons[i] = game.add.sprite(40 + 48 * (i + 1), 16, 'heart');
@@ -60,23 +68,29 @@ function create() {
 
 function update() {
 
+  //player movement
   if(this.cursors.up.isDown || this.keyW.isDown){
     player.body.y -= 4;
   }else if(this.cursors.down.isDown || this.keyS.isDown){
     player.body.y += 4;
   }
 
+  //check for collision of spaceship and player
   game.physics.arcade.overlap(player, spaceships, hitSpaceship, null, this);
 
+  //reset spaceship to right of screen off the left of the screen
   spaceships.forEach(checkShipPosition, this, true);
 
+  //give the spaceships a random amount for each time the velocity is reset
   spacetime = randomNum(5, 10) / 10;
 
+  //make sure no velocity is being acted upon the player throughout the game
   player.body.velocity.setTo(0,0);
 
+  //update score
   scoreText.text = "Score: " + score;
   score++;
-  scoreTracker++;
+  scoreTracker++;             //increase spaceship speed every 500 points
   if (scoreTracker > 500){
     if(score < 5000){
       speedMultiplier += 0.25;
@@ -84,7 +98,6 @@ function update() {
       speedMultiplier += 0.1;
     }
     scoreTracker = 0;
-    console.log(speedMultiplier);
   }
 }
 
@@ -107,8 +120,24 @@ function hitSpaceship(player, ship){
   if (health == 0){
     game.state.restart();
   }
+
+  playerRed();
+  game.time.events.add(100, playerDefault, this);
+  game.time.events.add(100, playerRed, this);
+  game.time.events.add(100, playerDefault, this);
 }
 
 function randomNum(min, max){
   return Math.floor(Math.random()* (max - min + 1) + min);
+}
+
+function playerDefault(){
+  console.log("Switch to Default");
+  player.loadTexture('alien');
+  player.animations.play('jetpack', 10, true);
+}
+
+function playerRed(){
+  console.log("Switch to Red");
+  player.loadTexture('alienRed');
 }
